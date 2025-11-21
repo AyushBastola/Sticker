7fb2f06a06dd77f4065bf79f388655a7e4e7237b
Ayush <student.149220@worcesterschools.net>
2025-11-21 15:45:04 -0500

    Completed the grid

--- src/main/java/com/example/sticker/Sticker.java
+++ src/main/java/com/example/sticker/Sticker.java
@@ -14,7 +14,9 @@ public class Sticker extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Group group = new Group();
-
+        int sceneWidth = 2000; // Width of the window
+        int sceneHeight = 800; // Height of the window
+        double size = 170.0; // Arguments for the drawSmashBall method
 //        Stop[] stops = new Stop[] {
 //                new Stop(0.00, Color.RED),
 //                new Stop(0.15, Color.ORANGE),
@@ -31,19 +33,24 @@ public class Sticker extends Application {
 //                true,
 //                CycleMethod.NO_CYCLE,
 //                stops);
-        Stop[] stops = new Stop[] {
-                new Stop(0, Color.RED),
-                new Stop(1, Color.ORANGE),
-        };
+//        Stop[] stops = new Stop[] {
+//                new Stop(0, Color.RED),
+//                new Stop(1, Color.ORANGE),
+//        };
 //        LinearGradient background = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, stops);
 //        ball.setFill(background);
-        drawSmashBall(200, 200, 170, group);
-        Scene scene = new Scene(group, 400, 400);
+        for (int y = 170; y < sceneHeight; y += 400) {
+            for (int x = 170; x < sceneWidth; x += 400) {
+                drawSmashBall(x, y, size, group);
+            }
+        }
+
+        Scene scene = new Scene(group, sceneWidth, sceneHeight);
         stage.setTitle("Ayush's Sticker");
         stage.setScene(scene);
         stage.show();
     }
-    public void drawSmashBall(int x, int y, int size, Group group) {
+    public void drawSmashBall(int x, int y, double size, Group group) {
         Circle ball = new Circle();
         ball.setCenterX(x);
         ball.setCenterY(y);
@@ -53,15 +60,20 @@ public class Sticker extends Application {
         ball.setStroke(Color.BLACK);
         Polygon cross1 = new Polygon();
         cross1.getPoints().addAll(new Double[] {
-                x - 30.0, y - 170.0, // Top right point
-                x - 30.0, y + 170.0, // Bottom right point
-                x - 75.0, y + 170.0, // Bottom left point
-                x - 75.0, y - 170.0 // Top left point
+                x - 30.0, y - size, // Top right point
+                x - 30.0, y + size, // Bottom right point
+                x - 70.0, y + size, // Bottom left point
+                x - 70.0, y - size // Top left point
         });
         cross1.setFill(Color.BLACK);
-        Rectangle cross2 = new Rectangle(x - size, y + 30, size * 2, 20); // The x and y coordinates start off at the top left corner
+        Polygon cross2 = new Polygon();
+        cross2.getPoints().addAll(new Double[] {
+                x + size, y + 30.0, // Top right point
+                x + size, y + 50.0, // Bottom right point
+                x - size, y + 50.0, // Bottom left point
+                x - size, y + 30.0 // Top left point
+        });
         cross2.setFill(Color.BLACK);
-
         group.getChildren().addAll(ball, cross1, cross2);
     }
 }
